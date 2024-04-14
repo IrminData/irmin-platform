25bf7a16b9fc32989b01a16bb2b26deee59b43eb
timborovkov <tim.borovkov@icloud.com>
2024-04-14 21:32:42 +0300

    Create websiteCTASection.tsx

---
+++ irmin-console/src/components/websiteCTASection.tsx
@@ -0,0 +1,88 @@
+export default function WebsiteCTASection() {
+  return (
+    <section
+      className="py-24 bg-white overflow-hidden"
+      style={{
+        backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")',
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="container px-4 mx-auto">
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full md:w-1/2 px-4 mb-20 lg:mb-0">
+            <div className="max-w-md mx-auto">
+              <h2 className="mb-8 text-4xl md:text-5xl font-heading font-bold text-coolGray-900 md:leading-15">
+                Join 6,000+ startups growing with Flex
+              </h2>
+              <ul className="mb-8">
+                <li className="flex items-center mb-4">
+                  <img
+                    className="mr-3"
+                    src="flex-ui-assets/elements/cta/checkbox-green.svg"
+                    alt=""
+                  />
+                  <span className="text-lg md:text-xl font-heading text-coolGray-500">
+                    Mauris pellentesque congue libero nec
+                  </span>
+                </li>
+                <li className="flex items-center mb-4">
+                  <img
+                    className="mr-3"
+                    src="flex-ui-assets/elements/cta/checkbox-green.svg"
+                    alt=""
+                  />
+                  <span className="text-lg md:text-xl font-heading text-coolGray-500">
+                    Suspendisse mollis tincidunt
+                  </span>
+                </li>
+                <li className="flex items-center">
+                  <img
+                    className="mr-3"
+                    src="flex-ui-assets/elements/cta/checkbox-green.svg"
+                    alt=""
+                  />
+                  <span className="text-lg md:text-xl font-heading text-coolGray-500">
+                    Praesent varius justo vel justo pulvinar
+                  </span>
+                </li>
+              </ul>
+              <div className="flex flex-wrap items-center">
+                <a
+                  className="inline-flex items-center justify-center px-7 py-3 h-14 w-full mb-2 md:mb-0 md:mr-4 md:w-auto text-lg leading-7 text-coolGray-800 bg-white hover:bg-coolGray-100 font-medium focus:ring-2 focus:ring-coolGray-200 focus:ring-opacity-50 border-coolGray-200 border border-coolGray-100 rounded-md shadow-sm"
+                  href="#"
+                >
+                  Learn More
+                </a>
+                <a
+                  className="inline-flex items-center justify-center px-7 py-3 h-14 w-full md:w-auto text-lg leading-7 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-transparent rounded-md shadow-sm"
+                  href="#"
+                >
+                  Get Started
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-4">
+            <div className="relative max-w-max mx-auto">
+              <img
+                className="absolute top-0 right-0 -mt-6 lg:-mt-12 -mr-6 lg:-mr-12 w-20 lg:w-auto z-10"
+                src="flex-ui-assets/elements/circle3-yellow.svg"
+                alt=""
+              />
+              <img
+                className="absolute bottom-0 left-0 -mb-6 lg:-mb-10-ml-6 lg:-ml-12 w-20 lg:w-auto"
+                src="flex-ui-assets/elements/dots3-blue.svg"
+                alt=""
+              />
+              <img
+                className="relative"
+                src="flex-ui-assets/elements/cta/photo-laptop-ph.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
