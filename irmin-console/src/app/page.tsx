e45bc8e9451d063b8d7b711a6c71185800a3767e
timborovkov <tim.borovkov@icloud.com>
2024-04-14 21:33:02 +0300

    Add new sections to the home page

--- irmin-console/src/app/page.tsx
+++ irmin-console/src/app/page.tsx
@@ -1,9 +1,21 @@
-import WebsiteHero from "@/components/websiteHero";
+import WebsiteHeroSection from "@/components/websiteHeroSection";
+import WebsiteLogoCloudSection from "@/components/websiteLogoCloudSection";
+import WebsiteFeaturesSection from "@/components/websiteFeaturesSection";
+import WebsiteTestimonialsSection from "@/components/websiteTestimonialsSection";
+import WebsiteNumbersSection from "@/components/websiteNumbersSection";
+import WebsiteContentSection from "@/components/websiteContentSection";
+import WebsiteCTASection from "@/components/websiteCTASection";
 
 export default function Home() {
   return (
     <>
-      <WebsiteHero />
+      <WebsiteHeroSection />
+      <WebsiteLogoCloudSection />
+      <WebsiteFeaturesSection />
+      <WebsiteTestimonialsSection />
+      <WebsiteNumbersSection />
+      <WebsiteContentSection />
+      <WebsiteCTASection />
     </>
   );
 }
