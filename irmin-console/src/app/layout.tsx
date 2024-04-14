2cbd7045a1d547054f14a4b27d67210105e47056
timborovkov <tim.borovkov@icloud.com>
2024-04-14 21:07:52 +0300

    Create website footer component

--- irmin-console/src/app/layout.tsx
+++ irmin-console/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import WebsiteNavigation from "@/components/websiteNavigation";
+import WebsiteFooter from "@/components/websiteFooter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <WebsiteNavigation />
         {children}
+        <WebsiteFooter />
       </body>
     </html>
   );
