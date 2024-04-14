d13f4600b218e8e9be00481bfe38fe291ff2c0e8
timborovkov <tim.borovkov@icloud.com>
2024-04-14 21:12:55 +0300

    Create website hero section

--- irmin-console/src/app/page.tsx
+++ irmin-console/src/app/page.tsx
@@ -1,5 +1,9 @@
+import WebsiteHero from "@/components/websiteHero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <WebsiteHero />
+    </>
   );
 }
