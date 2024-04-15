e2e4fe5f8a2f4dad3b0cacb9b885e8924a97ac29
timborovkov <tim.borovkov@icloud.com>
2024-04-15 14:59:58 +0300

    Create contact page

---
+++ irmin-console/src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import WebsiteContactSection from "@/components/website/websiteContactSection";
+import WebsiteNewsletterSection from "@/components/website/websiteNewsletterSection";
+
+export default function Contact() {
+  return (
+    <>
+      <WebsiteContactSection />
+      <WebsiteNewsletterSection />
+    </>
+  );
+}
