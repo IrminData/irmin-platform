a3c1afecf9202c68c7045d1819ff4909dbea3eeb
timborovkov <tim.borovkov@icloud.com>
2024-04-20 18:05:03 +0300

    Add parts list to data set

--- irmin-console/src/components/datasetList.tsx
+++ irmin-console/src/components/datasetList.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { IoChevronDownOutline, IoChevronUpOutline } from "react-icons/io5";
 
 interface DataSet {
+  id: number;
   name: string;
   sourceWorkspace: string;
   status: "private" | "public" | "connected";
+  parts: string[];
 }
 
 interface DataSetListProps {
@@ -11,6 +16,15 @@ interface DataSetListProps {
 }
 
 const DataSetList: React.FC<DataSetListProps> = ({ dataSets }) => {
+  const [openRows, setOpenRows] = useState<Record<number, boolean>>({});
+
+  const toggleRow = (id: number) => {
+    setOpenRows((prevOpenRows) => ({
+      ...prevOpenRows,
+      [id]: !prevOpenRows[id],
+    }));
+  };
+
   return (
     <div className="py-8">
       <div className="overflow-x-auto">
@@ -30,98 +44,130 @@ const DataSetList: React.FC<DataSetListProps> = ({ dataSets }) => {
           </thead>
           <tbody>
             {dataSets.map((dataset, index) => (
-              <tr key={index} className="shadow">
-                <td className="px-6 py-4 text-lg">
-                  {dataset.name}
-                  <br />
-                  <span className="text-sm text-ash_gray">
-                    Source: {dataset.sourceWorkspace}
-                  </span>
-                </td>
-                <td className="px-6 py-4">
-                  {dataset.status === "private" ? (
-                    <span className="block py-2 px-2 max-w-36 text-base text-white text-center leading-6 bg-midnight_green rounded-full shadow-sm">
-                      Private
+              <>
+                <tr
+                  key={index}
+                  className={openRows[dataset.id] ? "" : "shadow"}
+                >
+                  <td className="px-6 py-4 text-lg">
+                    {dataset.name}
+                    <br />
+                    <span className="text-sm text-ash_gray">
+                      Source: {dataset.sourceWorkspace}
                     </span>
-                  ) : dataset.status === "public" ? (
-                    <span className="block py-2 px-2 max-w-36 text-base text-white text-center leading-6 bg-ash_gray rounded-full shadow-sm">
-                      Public
-                    </span>
-                  ) : (
-                    <span className="block py-2 px-2 max-w-36 text-base text-rich_black text-center leading-6 bg-beige rounded-full shadow-sm">
-                      Connected
-                    </span>
-                  )}
-                </td>
-                <td className="px-6 py-4 text-right">
-                  <div className="flex space-x-2 float-right">
-                    <a
-                      href="#"
-                      className="text-ash_gray hover:underline py-3 px-1"
-                    >
-                      Logs
-                    </a>
-                    {(dataset.status === "private" ||
-                      dataset.status === "public") && (
-                      <>
-                        <a
-                          href="#"
-                          className="text-ash_gray hover:underline py-3 px-1"
-                        >
-                          Edit
-                        </a>
-                        <a
-                          href="#"
-                          className="text-ash_gray hover:underline py-3 px-1"
-                        >
-                          Remove
-                        </a>
-                      </>
+                  </td>
+                  <td className="px-6 py-4">
+                    {dataset.status === "private" ? (
+                      <span className="block py-2 px-2 max-w-36 text-base text-white text-center leading-6 bg-midnight_green rounded-full shadow-sm">
+                        Private
+                      </span>
+                    ) : dataset.status === "public" ? (
+                      <span className="block py-2 px-2 max-w-36 text-base text-white text-center leading-6 bg-ash_gray rounded-full shadow-sm">
+                        Public
+                      </span>
+                    ) : (
+                      <span className="block py-2 px-2 max-w-36 text-base text-rich_black text-center leading-6 bg-beige rounded-full shadow-sm">
+                        Connected
+                      </span>
                     )}
-                    {dataset.status === "connected" && (
-                      <>
-                        <a
-                          href="#"
-                          className="text-ash_gray hover:underline py-3 px-1"
-                        >
-                          View listing
-                        </a>
-                        <a
-                          href="#"
-                          className="text-ash_gray hover:underline py-3 px-1"
-                        >
-                          Disconnect
-                        </a>
-                      </>
-                    )}
-
-                    <div className="pl-2">
+                  </td>
+                  <td className="px-6 py-4 text-right">
+                    <div className="flex space-x-2 float-right">
                       <a
-                        className="block py-2 px-5 mb-2 text-base w-44 text-white text-center leading-6 bg-midnight_green hover:bg-midnight_green-600 focus:ring-2 focus:ring-midnight_green-500 focus:ring-opacity-50 rounded-full shadow-sm"
                         href="#"
+                        className="text-ash_gray hover:underline py-3 px-1"
                       >
-                        View data
+                        Logs
                       </a>
-                      {dataset.status === "private" && (
+                      {(dataset.status === "private" ||
+                        dataset.status === "public") && (
+                        <>
+                          <a
+                            href="#"
+                            className="text-ash_gray hover:underline py-3 px-1"
+                          >
+                            Edit
+                          </a>
+                          <a
+                            href="#"
+                            className="text-ash_gray hover:underline py-3 px-1"
+                          >
+                            Remove
+                          </a>
+                        </>
+                      )}
+                      {dataset.status === "connected" && (
+                        <>
+                          <a
+                            href="#"
+                            className="text-ash_gray hover:underline py-3 px-1"
+                          >
+                            View listing
+                          </a>
+                          <a
+                            href="#"
+                            className="text-ash_gray hover:underline py-3 px-1"
+                          >
+                            Disconnect
+                          </a>
+                        </>
+                      )}
+
+                      <div className="pl-2">
                         <a
-                          className="block py-2 px-5 mb-2 text-base w-44 text-white text-center leading-6 bg-ash_gray-500 hover:bg-ash_gray-600 focus:ring-2 focus:ring-ash_gray-500 focus:ring-opacity-50 rounded-full shadow-sm"
+                          className="block py-2 px-5 mb-2 text-base w-44 text-white text-center leading-6 bg-midnight_green hover:bg-midnight_green-600 focus:ring-2 focus:ring-midnight_green-500 focus:ring-opacity-50 rounded-full shadow-sm"
                           href="#"
                         >
-                          Publish dataset
+                          View data
                         </a>
-                      )}
-                      {dataset.status === "public" && (
-                        <a
-                          className="block py-2 px-5 mb-2 text-base w-44 text-white text-center leading-6 bg-ash_gray-500 hover:bg-ash_gray-600 focus:ring-2 focus:ring-ash_gray-500 focus:ring-opacity-50 rounded-full shadow-sm"
-                          href="#"
-                        >
-                          View listing
-                        </a>
-                      )}
+                        {dataset.status === "private" && (
+                          <a
+                            className="block py-2 px-5 mb-2 text-base w-44 text-white text-center leading-6 bg-ash_gray-500 hover:bg-ash_gray-600 focus:ring-2 focus:ring-ash_gray-500 focus:ring-opacity-50 rounded-full shadow-sm"
+                            href="#"
+                          >
+                            Publish dataset
+                          </a>
+                        )}
+                        {dataset.status === "public" && (
+                          <a
+                            className="block py-2 px-5 mb-2 text-base w-44 text-white text-center leading-6 bg-ash_gray-500 hover:bg-ash_gray-600 focus:ring-2 focus:ring-ash_gray-500 focus:ring-opacity-50 rounded-full shadow-sm"
+                            href="#"
+                          >
+                            View listing
+                          </a>
+                        )}
+                      </div>
+                      <button
+                        onClick={() => toggleRow(dataset.id)}
+                        className="text-ash_gray hover:text-ash_gray-800 focus:outline-none"
+                      >
+                        {openRows[dataset.id] ? (
+                          <IoChevronUpOutline className="w-10 h-10" />
+                        ) : (
+                          <IoChevronDownOutline className="w-10 h-10" />
+                        )}
+                      </button>
                     </div>
-                  </div>
-                </td>
-              </tr>
+                  </td>
+                </tr>
+                {openRows[dataset.id] && (
+                  <tr className="shadow">
+                    <td colSpan={3} className="px-20 py-4">
+                      <ul>
+                        {dataset.parts.map((part, index) => (
+                          <li
+                            key={index}
+                            className="py-3 border-b border-color-ash_gray"
+                          >
+                            {part}
+                            
+                          </li>
+                        ))}
+                      </ul>
+                    </td>
+                  </tr>
+                )}
+              </>
             ))}
           </tbody>
         </table>
