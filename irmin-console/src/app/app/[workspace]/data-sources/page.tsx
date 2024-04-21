b7e72d2ac2e70a0df6293b1eac7b854515e74023
timborovkov <tim.borovkov@icloud.com>
2024-04-21 18:42:23 +0300

    Add example DataSourceList to the page

--- irmin-console/src/app/app/[workspace]/data-sources/page.tsx
+++ irmin-console/src/app/app/[workspace]/data-sources/page.tsx
@@ -1,9 +1,46 @@
 import AppTitle from "@/components/appTitle";
+import DataSourceList from "@/components/dataSourceList";
 
 export default function DataSourcesPage() {
   return (
     <>
       <AppTitle title="Data sources" />
+      <DataSourceList
+        dataSources={[
+          {
+            id: 0,
+            name: "ExampleAnalyticsSync1",
+            connector: "Google Analytics",
+            nextSync: "in 3 hours",
+            nextSyncTimestamp: new Date(),
+            status: "running",
+          },
+          {
+            id: 1,
+            name: "Main Ads account sync",
+            connector: "Google AdSense",
+            nextSync: "in 8 hours",
+            nextSyncTimestamp: new Date(),
+            status: "errors",
+          },
+          {
+            id: 2,
+            name: "App database",
+            connector: "MySQL",
+            nextSync: "in 10 minutes",
+            nextSyncTimestamp: new Date(),
+            status: "stopped",
+          },
+          {
+            id: 3,
+            name: "Main Meta ads",
+            connector: "Facebook Ads",
+            nextSync: "in 30 minutes",
+            nextSyncTimestamp: new Date(),
+            status: "running",
+          },
+        ]}
+      />
     </>
   );
 }
