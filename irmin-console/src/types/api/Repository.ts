6976d765198cbf83877ce36506f835147d1510f7
Tim Borovkov <tim.borovkov@icloud.com>
2024-08-08 23:12:40 +0200

    Repository param immutable -> is_immutable (#67)

--- irmin-console/src/types/api/Repository.ts
+++ irmin-console/src/types/api/Repository.ts
@@ -14,7 +14,7 @@ import { WorkspaceUser } from '@/types/api/Workspace';
  * @typeParam description - Short description of the Repository
  * @typeParam documentation - Markdown documentation of the Repository. Allows for users to add explanations, examples, etc.
  * @typeParam workflow - The workflow (with workflowable) that has created this Repository. If created manually, this will be null.
- * @typeParam immutable - If the Repository is immutable, it cannot be changed or updated
+ * @typeParam is_immutable - If the Repository is_immutable, it cannot be changed or updated
  * @typeParam owner - The user within the workspace that owns the Repository and is responsible for it
  * @typeParam tables - A list of human readable table names that are part of this Repository
  * @typeParam created_at - Timestamp of the creation of the Repository
@@ -27,7 +27,7 @@ export interface Repository {
   description: string;
   documentation: string;
   workflow?: Workflow | null;
-  immutable: boolean;
+  is_immutable: boolean;
   owner: WorkspaceUser;
   tables: string[];
   created_at: string;
