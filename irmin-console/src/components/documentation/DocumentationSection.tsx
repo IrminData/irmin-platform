5dc4ae56c48746c5fd40c0f7975abbb846387aba
Tim Borovkov <tim.borovkov@icloud.com>
2024-09-01 19:43:01 +0300

    Update DocumentationSection.tsx

--- irmin-console/src/components/documentation/DocumentationSection.tsx
+++ irmin-console/src/components/documentation/DocumentationSection.tsx
@@ -3,12 +3,19 @@
 import PortalTitle from '@/components/portal/PortalTitle';
 
 import { useLocale } from '@/context/LocaleContext';
+import { useWorkspace } from '@/context/workspace';
 
 /**
  * Page UI to show the full documentation for the workspace
  */
 export default function DocumentationSection() {
   const { dict } = useLocale();
+  const {
+    connections: { connections },
+    exports: { exports },
+    actions: { actions },
+    repositories: { repositories },
+  } = useWorkspace();
 
   return (
     <div className='px-2 md:px-4'>
