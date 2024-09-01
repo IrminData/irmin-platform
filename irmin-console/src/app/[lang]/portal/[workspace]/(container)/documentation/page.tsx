6ac3b8f26be8c6546bb2ccc9d5140c08213eaa50
Tim Borovkov <tim.borovkov@icloud.com>
2024-09-01 19:01:58 +0300

    wip

--- irmin-console/src/app/[lang]/portal/[workspace]/(container)/documentation/page.tsx
+++ irmin-console/src/app/[lang]/portal/[workspace]/(container)/documentation/page.tsx
@@ -5,15 +5,16 @@ import PortalTitle from '@/components/portal/PortalTitle';
 import { useLocale } from '@/context/LocaleContext';
 
 /**
- * Logs page - home page for all logs
+ * Page to show the full documentation for the workspace
  *
  * @todo Implement this page and UI
  */
-export default function LogsPage() {
+export default function DocumentationPage() {
   const { dict } = useLocale();
+
   return (
     <div className='px-2 md:px-4'>
-      <PortalTitle title={dict.portalNavigation.links.logs} />
+      <PortalTitle title={dict.portalNavigation.links.documentation} />
     </div>
   );
 }
