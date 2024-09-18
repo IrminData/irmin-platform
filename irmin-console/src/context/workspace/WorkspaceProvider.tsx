c7cd8294271e318b509044b5ff6596917df26640
Tim Borovkov <tim.borovkov@icloud.com>
2024-09-18 03:39:35 +0300

    Update WorkspaceProvider.tsx (#126)

--- irmin-console/src/context/workspace/WorkspaceProvider.tsx
+++ irmin-console/src/context/workspace/WorkspaceProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 
 import { useParams } from 'next/navigation';
 
