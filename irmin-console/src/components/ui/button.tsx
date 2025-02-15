bd0e2ccd4d2527482621965f7a8a7419960b4ab2
Tim Borovkov <tim.borovkov@icloud.com>
2025-02-15 19:57:24 +0200

    Update button.tsx (#367)

--- irmin-console/src/components/ui/button.tsx
+++ irmin-console/src/components/ui/button.tsx
@@ -12,7 +12,7 @@ import { cn } from '@/utils/tw';
 import ButtonWithTooltip from './ButtonWithTooltip';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-normal transition-all focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex cursor-pointer items-center justify-center whitespace-nowrap rounded-md text-sm font-normal transition-all focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
