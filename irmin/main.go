d2e4c5233610abb076d829e60ac3a368bf1cf003
Tim Borovkov <tim.borovkov@icloud.com>
2025-04-07 21:29:36 -0700

    Tim/fiber middlewares (#100)
    * Return request ID in the response headers
    
    * Compress responses
    
    * Cache responses for 10 seconds
    
    * Update main.go

--- irmin/main.go
+++ irmin/main.go
@@ -6,8 +6,14 @@ import (
 	"irmin-api/routes"
 	"irmin-api/utils"
 	"log"
+	"net/http"
+	"sort"
+	"time"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cache"
+	"github.com/gofiber/fiber/v3/middleware/compress"
+	"github.com/gofiber/fiber/v3/middleware/requestid"
 )
 
 func main() {
@@ -43,6 +49,47 @@ func main() {
 
 	// Initialize a new Fiber app
 	app := fiber.New()
+
+	// Return request ID in the response headers
+	app.Use(requestid.New())
+
+	// Compress responses
+	app.Use(compress.New(
+		compress.Config{
+			Level: compress.LevelBestSpeed,
+		},
+	))
+
+	// Cache responses for 10 seconds for GET, HEAD, and OPTIONS methods.
+	// Cache key is generated based on the request path, query parameters, and authorization header.
+	app.Use(cache.New(
+		cache.Config{
+			Expiration: 10 * time.Second,
+			Methods: []string{
+				http.MethodGet,
+				http.MethodHead,
+				http.MethodOptions,
+			},
+			KeyGenerator: func(c fiber.Ctx) string {
+				queriesMap := c.Queries()
+				var keys []string
+				for key := range queriesMap {
+					keys = append(keys, key)
+				}
+				sort.Strings(keys)
+				queries := ""
+				for _, key := range keys {
+					value := queriesMap[key]
+					if queries == "" {
+						queries = key + "=" + value
+					} else {
+						queries += "&" + key + "=" + value
+					}
+				}
+				return c.Path() + queries + c.Get("authorization")
+			},
+		},
+	))
 
 	// Register the API routes
 	routes.RegisterAPIRoutes(app)
