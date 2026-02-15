138207a832ea4615b0c3a141e5849fc62dc9fe58
Tim Borovkov <tim.borovkov@icloud.com>
2026-02-15 15:15:28 +0100

    Reorder providers

--- irmin-console/src/app/layout.tsx
+++ irmin-console/src/app/layout.tsx
@@ -78,9 +78,9 @@ export default async function RootLayout(props: {
           <ClerkProvider dynamic>
             <ReactQueryProvider>
               <LocaleProvider>
-                <IAMProvider>
-                  <IrminCoreProvider>
-                    <PopupProvider>
+                <PopupProvider>
+                  <IAMProvider>
+                    <IrminCoreProvider>
                       <ThemeProvider
                         attribute='class'
                         defaultTheme='system'
@@ -89,9 +89,9 @@ export default async function RootLayout(props: {
                       >
                         {children}
                       </ThemeProvider>
-                    </PopupProvider>
-                  </IrminCoreProvider>
-                </IAMProvider>
+                    </IrminCoreProvider>
+                  </IAMProvider>
+                </PopupProvider>
               </LocaleProvider>
             </ReactQueryProvider>
           </ClerkProvider>
